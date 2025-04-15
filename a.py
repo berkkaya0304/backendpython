116588a28068bc9709fbfe8dbf6cf49c9a07b869
Berk Kaya <93735175+berkkaya0304@users.noreply.github.com>
2025-04-15 17:52:16 +0200

    Create a.py

---
+++ a.py
@@ -0,0 +1,52 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# Sample in-memory data store
+data = {
+    "1": {"name": "John Doe", "age": 30},
+    "2": {"name": "Jane Doe", "age": 25}
+}
+
+# API endpoint to get all users
+@app.route('/users', methods=['GET'])
+def get_users():
+    return jsonify(data)
+
+# API endpoint to get a user by ID
+@app.route('/users/<id>', methods=['GET'])
+def get_user(id):
+    if id in data:
+        return jsonify(data[id])
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+# API endpoint to create a new user
+@app.route('/users', methods=['POST'])
+def create_user():
+    user = request.json
+    id = str(len(data) + 1)
+    data[id] = user
+    return jsonify(data[id]), 201
+
+# API endpoint to update a user
+@app.route('/users/<id>', methods=['PUT'])
+def update_user(id):
+    if id in data:
+        user = request.json
+        data[id] = user
+        return jsonify(data[id])
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+# API endpoint to delete a user
+@app.route('/users/<id>', methods=['DELETE'])
+def delete_user(id):
+    if id in data:
+        del data[id]
+        return jsonify({"message": "User deleted"})
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
